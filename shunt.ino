a3ef3e80d6de5a99a08806e00de72a32b319bcaf
Shaun Gill <shaung75@gmail.com>
2024-09-23 22:24:46 +0100

    Adding in extra polling as surrounding environment kept giving false positives on IR sensors

--- shunt.ino
+++ shunt.ino
@@ -12,6 +12,14 @@ int T1C = 23;
 int T2T = 24;
 int T2C = 25;
 
+// Set all sensor variables to false
+Sensor1 = false;
+Sensor2 = false;
+Sensor3 = false;
+Sensor4 = false;
+
+int sensorCount = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,7 +49,29 @@ void loop() {
   motor.run(FORWARD);
   motor.setSpeed(255);
   
-  while(digitalRead(S4) != LOW){}
+  //while(digitalRead(S4) != LOW){}
+  
+  while(Sensor4 == false){
+    // If sensor is triggerd
+    if(digitalRead(S4) != LOW) {
+      // Poll another 10 times to confirm
+      for(i = 0; i < 10; i++){
+        if(digitalRead(S4) != LOW){
+          sensorCount++;
+        }
+      }
+
+      // confirm positive 10 times
+      if(sensorCount == 10) {
+        Sensor4 = true;
+      }
+
+      // reset sensorCount
+      sensorCount = 0;
+    }
+  }
+  // reset sensor
+  Sensor4 = false;
 
   // Stop Loco
   motor.setSpeed(0);
@@ -54,7 +84,29 @@ void loop() {
   motor.run(BACKWARD);
   motor.setSpeed(255);
   
-  while(digitalRead(S1) != LOW){}
+  //while(digitalRead(S1) != LOW){}
+
+  while(Sensor1 == false){
+    // If sensor is triggerd
+    if(digitalRead(S1) != LOW) {
+      // Poll another 10 times to confirm
+      for(i = 0; i < 10; i++){
+        if(digitalRead(S1) != LOW){
+          sensorCount++;
+        }
+      }
+
+      // confirm positive 10 times
+      if(sensorCount == 10) {
+        Sensor1 = true;
+      }
+
+      // reset sensorCount
+      sensorCount = 0;
+    }
+  }
+  // reset sensor
+  Sensor1 = false;
 
   // Stop loco
   motor.setSpeed(0);
@@ -67,7 +119,29 @@ void loop() {
   motor.run(FORWARD);
   motor.setSpeed(255);
   
-  while(digitalRead(S3) != LOW){}
+  //while(digitalRead(S3) != LOW){}
+
+  while(Sensor3 == false){
+    // If sensor is triggerd
+    if(digitalRead(S3) != LOW) {
+      // Poll another 10 times to confirm
+      for(i = 0; i < 10; i++){
+        if(digitalRead(S3) != LOW){
+          sensorCount++;
+        }
+      }
+
+      // confirm positive 10 times
+      if(sensorCount == 10) {
+        Sensor3 = true;
+      }
+
+      // reset sensorCount
+      sensorCount = 0;
+    }
+  }
+  // reset sensor
+  Sensor3 = false;
 
   // Stop loco
   motor.setSpeed(0);
@@ -80,7 +154,29 @@ void loop() {
   motor.run(BACKWARD);
   motor.setSpeed(255);
   
-  while(digitalRead(S2) != LOW){}
+  //while(digitalRead(S2) != LOW){}
+
+  while(Sensor2 == false){
+    // If sensor is triggerd
+    if(digitalRead(S2) != LOW) {
+      // Poll another 10 times to confirm
+      for(i = 0; i < 10; i++){
+        if(digitalRead(S2) != LOW){
+          sensorCount++;
+        }
+      }
+
+      // confirm positive 10 times
+      if(sensorCount == 10) {
+        Sensor2 = true;
+      }
+
+      // reset sensorCount
+      sensorCount = 0;
+    }
+  }
+  // reset sensor
+  Sensor2 = false;
 
   // Stop loco
   motor.setSpeed(0);
